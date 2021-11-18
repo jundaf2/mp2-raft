a718de366c00d5f75955234ca352f57beebc4ab0
Feng <jundaf2@fa21-cs425-g59-09.cs.illinois.edu>
2021-11-18 07:20:51 -0600

    exe is in build

--- build/CMakeFiles/raft.dir/DependInfo.cmake
+++ build/CMakeFiles/raft.dir/DependInfo.cmake
@@ -1,20 +1,20 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/hgfs/VMware_share_file/CS425_DS_CPP/mp2-raft/main.cpp" "/mnt/hgfs/VMware_share_file/CS425_DS_CPP/mp2-raft/build/CMakeFiles/raft.dir/main.cpp.o"
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jundaf2/mp2-raft/main.cpp" "/home/jundaf2/mp2-raft/build/CMakeFiles/raft.dir/main.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+SET(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- build/CMakeFiles/raft.dir/cmake_clean.cmake
+++ build/CMakeFiles/raft.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/raft.dir/main.cpp.o"
+FILE(REMOVE_RECURSE
+  "CMakeFiles/raft.dir/main.o"
   "raft.pdb"
   "raft"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/raft.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/raft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
